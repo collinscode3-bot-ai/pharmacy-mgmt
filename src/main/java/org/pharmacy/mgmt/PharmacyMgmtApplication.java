ef7144cd48e9214501ee3c8e8942a2081834e979
collinscode3-bot-ai <collinscode3@gmail.com>
2026-04-07 11:33:50 +0530

    Initial commit

---
+++ src/main/java/org/pharmacy/mgmt/PharmacyMgmtApplication.java
@@ -0,0 +1,13 @@
+package org.pharmacy.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyMgmtApplication.class, args);
+	}
+
+}
